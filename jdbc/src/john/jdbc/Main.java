f063d59650293a1f74c025c56711158eef53c4a0
宋方华 <songfh@sunits.com>
2017-10-26 14:48:16 +0800

    project initializeds

--- jdbc/src/john/jdbc/Main.java
+++ jdbc/src/john/jdbc/Main.java
@@ -1,21 +1,33 @@
 package john.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import john.jdbc.tables.Tours;
+import john.jdbc.util.InputHelper;
 
 public class Main {
+	private static final String SQL = "SELECT * FROM state WHERE id <= ?";
 	public static void main(String[] args) throws SQLException {
 		ResultSet rs = null;
+		double id;
+		try {
+			id = InputHelper.getDoubleInput("Enter a id: ");
+		} catch (NumberFormatException e) {
+			System.err.println("Error: invalid number");
+			return;
+		}
 		try (
 			Connection conn = DBUtil.getConnection(DBType.MYSQL);
-			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			PreparedStatement stmt = conn.prepareStatement(
+					SQL,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
 				) {
-			stmt.setMaxRows(10);
-			rs = stmt.executeQuery("SELECT * FROM state");
+			stmt.setDouble(1, id);
+			rs = stmt.executeQuery();
 			Tours.displayData(rs);
 		} catch (SQLException e) {
 			DBUtil.processException(e);
