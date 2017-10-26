41b584c4420b5bb360039ef9b552703eee72ac3e
宋方华 <songfh@sunits.com>
2017-10-26 11:03:05 +0800

    project initializeds

---
+++ jdbc/src/john/jdbc/Main.java
@@ -0,0 +1,28 @@
+package john.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	private static final String CONN_STRING = "jdbc:mysql://localhost:3306/state";
+	public static void main(String[] args) throws SQLException {
+//		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("Connected!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
+
+}
